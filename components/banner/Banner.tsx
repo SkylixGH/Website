f442cc9d0b1415e41aacbe9043cf9aa054f9c285
XFaonAE <xfaonae@protonmail.com>
2022-02-28 13:24:06 -0500

    Added banner

---
+++ components/banner/Banner.tsx
@@ -0,0 +1,37 @@
+import styles from "./Banner.module.scss";
+import Image from 'next/image';
+
+interface Props {
+    /**
+     * The banner title
+     */
+    title: string;
+
+    /**
+     * The banner subtitle
+     */
+    subtitle?: string;
+
+    /**
+     * The banner background image URL
+     */
+    imageURL?: string;
+}
+
+export default function Banner(props: Props) {
+    return (
+        <div className={styles.root}>
+            { props.imageURL && <div className={styles.image}>
+                { /* eslint-disable-next-line @next/next/no-img-element */ }
+                <img src={props.imageURL} alt={props.title} />
+            </div> }
+
+            { props.imageURL && <div className={styles.cover} /> }
+
+            <div className={styles.text}>
+                <h1>{props.title}</h1>
+                {props.subtitle && <p>{props.subtitle}</p>}
+            </div>
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import Banner from "../components/banner/Banner";
+
 export default function Home() {
     return (
         <div>
-            <br />
+            <Banner imageURL="/HomeBanner.svg" title="S K Y L I X" />
 
-            <h1 style={{
+            {/* <h1 style={{
                 textAlign: "center",
             }}>S K Y L I X</h1>
 
@@ -11,7 +13,7 @@ export default function Home() {
                 textAlign: "center",
             }}>
                 Taking conversations to the moon! 🚀
-            </p>
+            </p> */}
 
             <h3>Skylix isn{ "'" }t ready yet!</h3>
             <p>Skylix is still in dev, check out our github for more info like our discord server, etc...</p>
